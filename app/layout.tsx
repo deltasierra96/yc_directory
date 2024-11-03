668c261dd09d594d9f7e6464e90cd7a24443cdc9
deltasierra96 <dylanstephens96@outlook.com>
2024-11-03 00:23:05 +0000

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
+
 import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
@@ -9,59 +10,59 @@ const workSans = localFont({
     {
       path: "./fonts/WorkSans-Black.ttf",
       weight: "900",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-ExtraBold.ttf",
       weight: "800",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-Bold.ttf",
       weight: "700",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-SemiBold.ttf",
       weight: "600",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-Medium.ttf",
       weight: "500",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-Regular.ttf",
       weight: "400",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-Black.ttf",
       weight: "900",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-Thin.ttf",
       weight: "200",
-      style: "normal",
+      style: "normal"
     },
     {
       path: "./fonts/WorkSans-ExtraLight.ttf",
       weight: "100",
-      style: "normal",
-    },
+      style: "normal"
+    }
   ],
-  variable: "--font-work-sans",
+  variable: "--font-work-sans"
 });
 
 export const metadata: Metadata = {
   title: "YC Directory",
-  description: "Pitch, Vote and Grow",
+  description: "Pitch, Vote and Grow"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
